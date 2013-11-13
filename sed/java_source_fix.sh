dd78556c641d4da5da66e51bcfb975d10ac1e0ed
吴杨进 <wuyjnuaa@126.com>
2013-11-13 13:01:33 +0800

    add 2 more liner

--- sed/java_source_fix.sh
+++ sed/java_source_fix.sh
@@ -2,3 +2,9 @@
 # do a common replacement in bunch of java source files
 find . -name '*.java' | xargs sed -i '/execute/{:loop;/throws Exception/!{N;b loop};s/)[\t\ ]*\r\?\n[\t\ ]*/) /g;s/Exception/Throwable/g}';
 find . -name '*.java' | xargs chmod u+w;
+
+# auto-generate some java Map related code
+find . -name "*.java" | sed -n 's/\.\/[[:alnum:]]\+\/F\([[:alnum:]_@]\+\)\.java/map\.put(\"\1\", F\1\.class)\;/g;p'
+
+# auto-generate java imports
+$ find . -name "*.java" | sed -n 's/\.\/\([[:alnum:]]\+\)\/F\([[:alnum:]_@]\+\)\.java/import com\.moran\.cep\.core\.executor\.expression\.function\.\1\.F\2\;/g;p'
